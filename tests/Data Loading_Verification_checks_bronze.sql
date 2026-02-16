0c8aa737b6c01e781378efd3721913afc58681f5
Mohammed Makhluti <171294504+Mohammed-Makhluti@users.noreply.github.com>
2026-02-16 10:03:25 +0300

    Refactor data loading verification for silver layer
    Updated procedure calls and added verification checks for silver layer.

--- tests/Data Loading_Verification_checks_bronze.sql
+++ tests/Data Loading_Verification_checks_bronze.sql
@@ -2,7 +2,7 @@
 -- Data Loading Verification Script (Bronze Layer)
 -- =============================================================================
 -- For Call procedure code:
-CALL bronze.load_bronze_layer();
+CALL bronze.load_bronze();
 -- =============================================================================
 
 -- 1. Check Row Counts for all tables
@@ -18,19 +18,32 @@ SELECT 'bronze.erp_loc_a101', COUNT(*) FROM bronze.erp_loc_a101
 UNION ALL
 SELECT 'bronze.erp_px_cat_g1v2', COUNT(*) FROM bronze.erp_px_cat_g1v2;
 
--- 2. Sample Check: View top 5 rows from Sales to ensure data alignment
-SELECT * FROM bronze.crm_sales_details LIMIT 5;
-
--- 3. Check for NULLs in Primary Keys (Quick Quality Check)
+-- 2. Check for NULLs in Primary Keys (Quick Quality Check)
 SELECT
     (SELECT COUNT(*) FROM bronze.crm_cust_info WHERE cst_id IS NULL) AS null_ids_crm,
     (SELECT COUNT(*) FROM bronze.crm_sales_details WHERE sls_ord_num IS NULL) AS null_orders_sales;
 
--- 4. Print Any Table:
+-- =============================================================================
+-- Data Loading Verification Script (silver Layer)
+-- =============================================================================
+-- For Call procedure code:
+CALL silver.load_silver();
+-- =============================================================================
 
-SELECT * FROM bronze.crm_prd_info LIMIT 10;
-SELECT * FROM bronze.crm_sales_details LIMIT 10;
-SELECT * FROM bronze.crm_sales_details LIMIT 10;
-SELECT * FROM bronze.erp_cus_az12 LIMIT 10;
-SELECT * FROM bronze.erp_loc_a101 LIMIT 10;
-SELECT * FROM bronze.erp_px_cat_g1v2 LIMIT 10;
+-- 1. Check Row Counts for all tables
+SELECT 'silver.crm_cust_info' AS table_name, COUNT(*) AS row_count FROM silver.crm_cust_info
+UNION ALL
+SELECT 'silver.crm_prd_info', COUNT(*) FROM silver.crm_prd_info
+UNION ALL
+SELECT 'silver.crm_sales_details', COUNT(*) FROM silver.crm_sales_details
+UNION ALL
+SELECT 'silver.erp_cus_az12', COUNT(*) FROM silver.erp_cus_az12
+UNION ALL
+SELECT 'silver.erp_loc_a101', COUNT(*) FROM silver.erp_loc_a101
+UNION ALL
+SELECT 'silver.erp_px_cat_g1v2', COUNT(*) FROM silver.erp_px_cat_g1v2;
+
+-- 2. Check for NULLs in Primary Keys (Quick Quality Check)
+SELECT
+    (SELECT COUNT(*) FROM silver.crm_cust_info WHERE cst_id IS NULL) AS null_ids_crm,
+    (SELECT COUNT(*) FROM silver.crm_sales_details WHERE sls_ord_num IS NULL) AS null_orders_sales;
