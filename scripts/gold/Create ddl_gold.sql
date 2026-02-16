9933011f0236689406bcee15b021aa2f3621adae
Mohammed Makhluti <171294504+Mohammed-Makhluti@users.noreply.github.com>
2026-02-16 23:47:24 +0300

    Add views for dim_products and fact_sales
    Created views for gold.dim_products and gold.fact_sales, including necessary joins and selections.

--- scripts/gold/Create ddl_gold.sql
+++ scripts/gold/Create ddl_gold.sql
@@ -40,9 +40,58 @@ LEFT JOIN silver.erp_cus_az12 ca
 LEFT JOIN silver.erp_loc_a101 la
     ON ci.cst_key = la.cid;
 
-SELECT * FROM gold.dim_customers; -- Test The dim customers View
+SELECT * FROM gold.dim_customers;
 
 -- =============================================================================
 -- Create Dimension: gold.dim_products
 -- =============================================================================
 
+DROP VIEW IF EXISTS gold.dim_products;
+CREATE OR REPLACE VIEW gold.dim_products AS
+SELECT
+    ROW_NUMBER() OVER (ORDER BY pn.prd_id) AS product_key,
+    -- ===
+    pn.prd_id AS product_id,
+    pn.prd_key As product_number,
+    pn.prd_nm AS product_name,
+    -- ===
+    pn.cat_id AS category_id,
+    COALESCE(pc.cat, 'n/a') AS category,
+    COALESCE(pc.subcat, 'n/a') AS subcategory,
+    COALESCE(pc.maintenance, 'n/a') AS maintenance,
+    -- ===
+    pn.prd_cost AS cost,
+    pn.prd_line AS product_line,
+    pn.prd_start_dt AS start_date
+    -- pn.prd_end_dt -- We Don't need It B/C Need Current Product
+FROM silver.crm_prd_info pn
+LEFT JOIN silver.erp_px_cat_g1v2 pc
+    ON pn.cat_id = pc.id
+WHERE pn.prd_end_dt IS NULL;
+
+-- =============================================================================
+-- Create Fact Table: gold.fact_sales
+-- =============================================================================
+
+DROP VIEW IF EXISTS gold.fact_sales;
+CREATE OR REPLACE VIEW gold.fact_sales AS
+SELECT
+    -- ===
+    sd.sls_ord_num AS order_number,
+    pr.product_key,
+    cu.customer_key,
+    -- sd.sls_prd_key, -- WE REPLACED BY pr.product_key,
+    -- sd.sls_cust_id, -- WEREPLACED BY dc.customer_key,
+    -- ===
+    sd.sls_order_dt AS order_date,
+    sd.sls_ship_dt AS ship_date,
+    sd.sls_due_dt AS due_date,
+    -- ===
+    sd.sls_sales AS sales,
+    sd.sls_quantity As quantity,
+    sd.sls_price AS price
+FROM silver.crm_sales_details AS sd
+LEFT JOIN gold.dim_customers AS cu
+    ON sd.sls_cust_id = cu.customer_id
+LEFT JOIN gold.dim_products AS pr
+    ON sd.sls_prd_key = pr.product_number;
